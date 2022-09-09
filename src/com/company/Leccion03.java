f1808ae67821b51ca6da3ba0878c1ad194547a6e
eglez7 <eglez77@gmail.com>
2022-09-09 20:41:08 +0200

    Ejercicio 3

--- src/com/company/Leccion03.java
+++ src/com/company/Leccion03.java
@@ -2,17 +2,23 @@ package com.company;
 
 public class Leccion03 {
     public static void main(String[] args) {
-        /*
-        Crear un bucle que permita concatenar textos y imprima el resultado final por consola.
-        Concatenar nombres
-        Los textos pueden venir de un array String
-        String[] nombres = {"", "", "", ""};
-        Separados por un espacio
-        Se puede usar el método concat o si no el operador + Ejemplo
-        "Pepe", "Juan", "Anthony" Resultado:
-        Pepe Juan Anthony (en la misma línea)
-         */
 
+        String [] nombres={"Pedro","Mario","Julia","Isabel","María"} ;
+        String resultadoFinal="";
 
+        System.out.println("Método con el operador +");
+        for(String nombre :  nombres){
+            resultadoFinal=resultadoFinal+" " + nombre;
+        }
+        System.out.println(resultadoFinal);
+
+        resultadoFinal="";
+        System.out.println("Método con la función concat");
+        for(String nombre :  nombres){
+            resultadoFinal=resultadoFinal.concat(" ");
+            resultadoFinal=resultadoFinal.concat(nombre);
+//            resultadoFinal=resultadoFinal+" " + nombre;
+        }
+        System.out.println(resultadoFinal);
     }
 }
