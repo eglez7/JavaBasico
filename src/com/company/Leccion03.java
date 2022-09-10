61126d45ef7414b25c28e08711a32784e0c715ab
eglez7 <eglez77@gmail.com>
2022-09-10 11:54:02 +0200

    Inicio ejercicio 4

--- src/com/company/Leccion03.java
+++ src/com/company/Leccion03.java
@@ -3,20 +3,20 @@ package com.company;
 public class Leccion03 {
     public static void main(String[] args) {
 
-        String [] nombres={"Pedro","Mario","Julia","Isabel","María"} ;
-        String resultadoFinal="";
+        String[] nombres = {"Pedro", "Mario", "Julia", "Isabel", "María"};
+        String resultadoFinal = "";
 
         System.out.println("Método con el operador +");
-        for(String nombre :  nombres){
-            resultadoFinal=resultadoFinal+" " + nombre;
+        for (String nombre : nombres) {
+            resultadoFinal = resultadoFinal + " " + nombre;
         }
         System.out.println(resultadoFinal);
 
-        resultadoFinal="";
+        resultadoFinal = "";
         System.out.println("Método con la función concat");
-        for(String nombre :  nombres){
-            resultadoFinal=resultadoFinal.concat(" ");
-            resultadoFinal=resultadoFinal.concat(nombre);
+        for (String nombre : nombres) {
+            resultadoFinal = resultadoFinal.concat(" ");
+            resultadoFinal = resultadoFinal.concat(nombre);
 //            resultadoFinal=resultadoFinal+" " + nombre;
         }
         System.out.println(resultadoFinal);
---
+++ src/com/company/Leccion04.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Leccion04 {
+    public static void main(String[] args) {
+        /*
+        En este ejercicio tendréis que crear una clase SmartDevice. Dentro crearéis las clases hijas: SmartPhone y SmartWatch.
+        Agregaréis atributos tal cual tendrían esos objetos en la realidad.
+        Crear constructor vacío y con todos los parámetros para cada clase.
+        Desde una clase Main: crearéis objetos de cada una y los utilizaréis para imprimir sus valores por consola.
+         */
+    }
+
+    public class  SmartDevice {
+
+    }
+
+    public class SmartPhone extends SmartDevice{
+
+    }
+
+    public class SmartWatch extends SmartDevice{
+
+    }
+
+}
