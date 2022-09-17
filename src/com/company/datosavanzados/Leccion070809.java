6e319af15a191306147c4ffe2c26f337787a3dfb
eglez7 <eglez77@gmail.com>
2022-09-17 11:26:29 +0200

    Ejercicio 9 completo

--- src/com/company/datosavanzados/Leccion070809.java
+++ src/com/company/datosavanzados/Leccion070809.java
@@ -1,12 +1,14 @@
 package com.company.datosavanzados;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.Scanner;
 import java.util.Vector;
 
 
 public class Leccion070809 {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         /*
 
 
@@ -47,6 +49,7 @@ public class Leccion070809 {
 
         // 9. Utilizando InputStream y PrintStream, crea una función que reciba dos parámetros: "fileIn" y "fileOut".
         // La tarea de la función será realizar la copia del fichero dado en el parámetro "fileIn" al fichero dado en "fileOut".
+        //copiarFicheros("d:/fichero.txt","d:/fichero_salida.txt");
 
 
         // 10. Sorpréndenos creando un programa de tu elección
@@ -54,6 +57,22 @@ public class Leccion070809 {
 
 
 
+
+    }
+
+    private static void copiarFicheros(String fileIn, String fileOut) {
+        try {
+            InputStream fichero = new FileInputStream(fileIn);
+            byte[] datos = fichero.readAllBytes();
+            fichero.close();
+
+            PrintStream ficheroSalida = new PrintStream(fileOut);
+            ficheroSalida.write(datos);
+            ficheroSalida.close();
+
+        }  catch (IOException e) {
+            System.out.println("La has fastidiado majo " + e.getMessage());
+        }
     }
 
     private static void dividiendoPorCero() {
